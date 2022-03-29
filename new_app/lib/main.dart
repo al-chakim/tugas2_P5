0e25beaed1b0a5dd620f464705b4978f3d020c79
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-03-29 16:59:48 +0700

    cape

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -1,12 +1,101 @@
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+
+    Widget kontener(){
+      return Container(
+        padding: EdgeInsets.only(
+          top: 15,
+          bottom: 15,
+          left: 23,
+          right: 15,
+        ),
+        margin: EdgeInsets.only(
+          top: 15,
+          bottom: 5,
+          right: 25,
+          left: 25
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xff1BA098),
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Column(
+          children: [
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'NPM',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                SizedBox(width: 170,),
+                Text(
+                  '065119167',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                SizedBox(width: 8,),
+                Icon(
+                  Icons.copy,
+                  size: 15,
+                  color: Colors.white,
+                )
+              ],
+            ),
+            SizedBox(height: 5,),
+            Row(
+              children: [
+                Text(
+                  'Status Keatifan',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                SizedBox(width: 170,),
+                Text(
+                  'Aktif',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 5,),
+            Row(
+              children: [
+                Text(
+                  'Program Studi',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                SizedBox(width: 115,),
+                Text(
+                  'Ilmu Komputer'
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    }
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -43,9 +132,24 @@ class MyApp extends StatelessWidget {
                     'assets/profile.png',
                     width: 135,
                     height: 135,
+                  ),
+                  SizedBox(height: 15,),
+                  Text(
+                    'Muchammad Amru Al-Chakim',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  Text(
+                    'amru.065119167@unpak.ac.id',
+                  ),
+                  Text(
+                    '085775190991',
                   )
                 ],
-              )
+              ),
+              kontener(),
             ],
           )
         ),
