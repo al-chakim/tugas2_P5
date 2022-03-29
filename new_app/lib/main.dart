f12ab301b8a2384c758b19a649ecccc329126f25
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-03-29 11:51:04 +0700

    belajar

---
+++ new_app/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: ListView(
+            children: [
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: (){}, 
+                    icon: Icon(Icons.arrow_back)
+                  ),
+                  //SizedBox(width: 5),
+                  Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: (){}, 
+                    icon: Icon(Icons.edit)
+                  )
+                ],
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
